43b9bf7a84e66330618e7714be219a9b5dab7899
xiongliangshan <xiongliangshan@lyancoffee.com>
2016-07-12 18:56:26 +0800

    测试提交

--- CoffeeShop/app/src/main/java/com/xls/http/HttpUtils.java
+++ CoffeeShop/app/src/main/java/com/xls/http/HttpUtils.java
@@ -16,6 +16,7 @@ public class HttpUtils {
     public static String BASE_URL = "http://mtest.lyancafe.com/shop/v3/";
 
 //    public static String BASE_URL = "http://192.168.1.92:8080/shop/v3/";
+    //
 
 
     /**
