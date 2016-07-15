600a0f3158ffe43ec1c6e13f88eae782d1c2c668
xiongliangshan <xiongliangshan@lyancoffee.com>
2016-07-15 16:43:09 +0800

    集成单元测试

--- CoffeeShop/app/src/test/java/com/lyancafe/coffeeshop/helper/OrderHelperTest.java
+++
@@ -1,8 +0,0 @@
-import static org.junit.Assert.*;
-
-/**
- * Created by Administrator on 2016/7/15.
- */
-public class OrderHelperTest {
-
-}
---
+++ CoffeeShop/app/src/test/java/com/lyancafe/coffeeshop/helper/PrintHelperTest.java
@@ -0,0 +1,16 @@
+package com.lyancafe.coffeeshop.helper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2016/7/15.
+ */
+public class PrintHelperTest {
+
+    @Test
+    public void testGetMaterialContent() throws Exception {
+
+    }
+}
