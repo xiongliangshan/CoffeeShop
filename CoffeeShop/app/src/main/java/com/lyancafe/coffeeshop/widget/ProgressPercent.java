a5d6cb06c31cdea0f8a58e8bde35383ba096e9d7
xiongliangshan <xiongliangshan@lyancoffee.com>
2017-12-28 10:51:27 +0800

    优化自定义接单送达进度控件

--- CoffeeShop/app/src/main/java/com/lyancafe/coffeeshop/widget/ProgressPercent.java
+++ CoffeeShop/app/src/main/java/com/lyancafe/coffeeshop/widget/ProgressPercent.java
@@ -30,19 +30,17 @@ public class ProgressPercent extends View {
     private int mDesTextColor;
     private float mDesTextSize;
     private int mPassColor;
-    private int mUnPassColor;
     private Paint textPaint;
     private Paint rectPaint;
 
-    //起止时间
-    private long startTime;
-
-    //截止时间
-    private long endTime;
 
     private int passMinute;
     private int unPassMinute;
     private int overMinute;
+
+    private RectF mProgressRect = null;
+    private RectF mPassRect = null;
+    private RectF mUnpassRect = null;
 
     /**
      * -1:当前时间小于起始时间
@@ -64,7 +62,6 @@ public class ProgressPercent extends View {
         mDesTextColor = a.getColor(R.styleable.ProgressPercent_desTextColor, Color.BLACK);
         mDesTextSize = a.getDimension(R.styleable.ProgressPercent_desTextSize,16f);
         mPassColor = a.getColor(R.styleable.ProgressPercent_passColor,Color.GREEN);
-        mUnPassColor = a.getColor(R.styleable.ProgressPercent_unpassColor,Color.GRAY);
         a.recycle();
         initDraw();
     }
@@ -106,8 +103,6 @@ public class ProgressPercent extends View {
      * @param endTime
      */
     public void updateProgress(long startTime,long endTime){
-        this.startTime = startTime;
-        this.endTime = endTime;
         long nowTime = System.currentTimeMillis();
         if(nowTime<=startTime){
            this.status = -1;
@@ -120,9 +115,10 @@ public class ProgressPercent extends View {
             overMinute = (int) (Math.abs(endTime-nowTime)/(60*1000));
             mOverText = "超时 "+overMinute+" 分钟";
         }
+
         invalidate();
 
-        LogUtil.d(TAG,"status = "+status+"|pass ="+passMinute+"|unpass="+unPassMinute+"|over ="+overMinute);
+        LogUtil.d(TAG,"acceptTime = "+startTime+"|status = "+status+"|pass ="+passMinute+"|unpass="+unPassMinute+"|over ="+overMinute);
     }
 
 
@@ -157,12 +153,12 @@ public class ProgressPercent extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
         int paddingLeft = getPaddingLeft();
         int paddingRight = getPaddingRight();
         int paddingTop = getPaddingTop();
         int paddingBottom = getPaddingBottom();
 
-        int width = getWidth() - paddingLeft - paddingRight;
         int height = getHeight() - paddingTop - paddingBottom;
 
         float startTextWidth = textPaint.measureText(mStartText);
@@ -170,25 +166,41 @@ public class ProgressPercent extends View {
         Paint.FontMetrics fontMetrics = textPaint.getFontMetrics();
         float top = fontMetrics.top;//为基线到字体上边框的距离,即上图中的top
         float bottom = fontMetrics.bottom;//为基线到字体下边框的距离,即上图中的bottom
-        int baseLineY = (int) (paddingTop + height/2 - top/2 - bottom/2);//基线中间点的y轴计算公式
+        float baseLineY = paddingTop + height/2 - top/2 - bottom/2;//基线中间点的y轴计算公式
+
         textPaint.setColor(Color.BLACK);
-        canvas.drawText(mStartText,paddingLeft+2,baseLineY,textPaint);
-        canvas.drawText(mEndText,getWidth()-paddingRight-endTextWidth-2,baseLineY,textPaint);
-        RectF progressRect = new RectF(paddingLeft+startTextWidth+4,paddingTop,getWidth()-paddingRight-endTextWidth-4,height);
+        canvas.drawText(mStartText,getPaddingLeft()+2,baseLineY,textPaint);
+        canvas.drawText(mEndText,getWidth()-getPaddingRight()-endTextWidth-2,baseLineY,textPaint);
+        if(mProgressRect==null){
+            mProgressRect = new RectF(paddingLeft+startTextWidth+4,paddingTop,getWidth()-paddingRight-endTextWidth-4,height);
+        }else {
+            mProgressRect.set(paddingLeft+startTextWidth+4,paddingTop,getWidth()-paddingRight-endTextWidth-4,height);
+        }
+
         if(status==-1){
             //未开始
             rectPaint.setStyle(Paint.Style.STROKE);
             rectPaint.setAntiAlias(true);
             rectPaint.setStrokeWidth(1f);
             rectPaint.setColor(Color.BLACK);
-            canvas.drawRoundRect(progressRect,20,20,rectPaint);
+            canvas.drawRoundRect(mProgressRect,20,20,rectPaint);
             textPaint.setColor(Color.BLACK);
             float beforeStartWidth = textPaint.measureText(mBeforeStartText);
-            canvas.drawText(mBeforeStartText,progressRect.left+(progressRect.width()-beforeStartWidth)/2.0f,baseLineY,textPaint);
+            canvas.drawText(mBeforeStartText,mProgressRect.left+(mProgressRect.width()-beforeStartWidth)/2.0f,baseLineY,textPaint);
         }else if(status==0){
             //进行中
-            RectF rectPass = new RectF(progressRect.left+1,progressRect.top+1,progressRect.width()*passMinute/(passMinute+unPassMinute)-1,progressRect.bottom-1);
-            RectF rectUnPass = new RectF(paddingLeft+width-rectPass.right+1,paddingTop+1,width-1,height-1);
+            if(mPassRect==null){
+                mPassRect = new RectF(mProgressRect.left+1,mProgressRect.top+1,mProgressRect.left+mProgressRect.width()*passMinute/(passMinute+unPassMinute)+1,mProgressRect.bottom-1);
+            }else{
+                mPassRect.set(mProgressRect.left+1,mProgressRect.top+1,mProgressRect.left+mProgressRect.width()*passMinute/(passMinute+unPassMinute)+1,mProgressRect.bottom-1);
+            }
+//            RectF rectPass = new RectF(progressRect.left+1,progressRect.top+1,progressRect.left+progressRect.width()*passMinute/(passMinute+unPassMinute)+1,progressRect.bottom-1);
+//            RectF rectUnPass = new RectF(rectPass.right+1,paddingTop+1,progressRect.right-1,height-1);
+            if(mUnpassRect==null){
+                mUnpassRect = new RectF(mPassRect.right+1,paddingTop+1,mProgressRect.right-1,height-1);
+            }else {
+                mUnpassRect.set(mPassRect.right+1,paddingTop+1,mProgressRect.right-1,height-1);
+            }
 
             rectPaint.setStyle(Paint.Style.STROKE);
             rectPaint.setStrokeJoin(Paint.Join.ROUND);
@@ -196,27 +208,27 @@ public class ProgressPercent extends View {
             rectPaint.setDither(true);
             rectPaint.setStrokeWidth(1f);
             rectPaint.setColor(Color.BLACK);
-            canvas.drawRoundRect(progressRect,20,20,rectPaint);
+            canvas.drawRoundRect(mProgressRect,20,20,rectPaint);
 
             rectPaint.setStyle(Paint.Style.FILL);
             rectPaint.setColor(mPassColor);
-            canvas.drawRoundRect(rectPass,20,20,rectPaint);
+            canvas.drawRoundRect(mPassRect,20,20,rectPaint);
 
             float passTextWidth = textPaint.measureText(String.valueOf(passMinute));
             textPaint.setColor(Color.BLACK);
-            canvas.drawText(String.valueOf(passMinute),rectPass.left+(rectPass.width()-passTextWidth)/2.0f,baseLineY,textPaint);
+            canvas.drawText(String.valueOf(passMinute),mPassRect.left+(mPassRect.width()-passTextWidth)/2.0f,baseLineY,textPaint);
 
 
             float unPassTextWidth = textPaint.measureText(String.valueOf(unPassMinute));
-            canvas.drawText(String.valueOf(unPassMinute),rectUnPass.left+(rectUnPass.width()-unPassTextWidth)/2.0f,baseLineY,textPaint);
+            canvas.drawText(String.valueOf(unPassMinute),mUnpassRect.left-1+(mUnpassRect.width()-unPassTextWidth)/2.0f,baseLineY,textPaint);
         }else{
             //已超时
             rectPaint.setStyle(Paint.Style.FILL_AND_STROKE);
             rectPaint.setColor(Color.RED);
-            canvas.drawRoundRect(progressRect,24,24,rectPaint);
+            canvas.drawRoundRect(mProgressRect,24,24,rectPaint);
             textPaint.setColor(Color.WHITE);
             float overTextWidth = textPaint.measureText(mOverText);
-            canvas.drawText(mOverText,progressRect.left+(progressRect.width()-overTextWidth)/2.0f,baseLineY,textPaint);
+            canvas.drawText(mOverText,mProgressRect.left+(mProgressRect.width()-overTextWidth)/2.0f,baseLineY,textPaint);
         }
     }
 }
